35caa8a24fe2946298cc3fd5fc3f3e472cf52520
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-01-05 17:23:17 +0600

    add efi script

---
+++ efi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+timedatectl set-ntp true
+parted -s /dev/sda mklabel gpt mkpart primary 2MiB 512MiB mkpart primary 512MiB 100% set 1 esp on
+mkfs.ext4 /dev/sda2
+mkfs.fat -F32 /dev/sda1
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+pacstrap /mnt base linux linux-firmware networkmanager sudo grub efibootmgr
+genfstab -U /mnt >> /mnt/etc/fstab
+cat << EOF > /mnt/configure.sh
+ln -sf /usr/share/zoneinfo/Asia/Omsk /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+systemctl enable NetworkManager
+useradd -s /bin/bash -mG wheel user
+passwd user
+grub-install
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+chmod 755 /mnt/configure.sh
+arch-chroot /mnt /configure.sh
+rm -rf /mnt/configure.sh
+umount -R /mnt
+reboot

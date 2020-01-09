7393d8a3e3dd7fe291d55919c01dbe90f5b1eb3d
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-01-09 19:21:02 +0600

    add android build env installation

--- efi.sh
+++ efi.sh
@@ -7,7 +7,7 @@ mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-pacstrap /mnt base linux linux-firmware networkmanager sudo grub efibootmgr base-devel nano wget openssh
+pacstrap /mnt base linux linux-firmware networkmanager sudo grub efibootmgr base-devel nano wget openssh jenkins
 genfstab -U /mnt >> /mnt/etc/fstab
 cat << EOF > /mnt/configure.sh
 ln -sf /usr/share/zoneinfo/Asia/Omsk /etc/localtime
@@ -20,7 +20,8 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 systemctl enable NetworkManager
 systemctl enable sshd
-useradd -s /bin/bash -mG wheel user
+systemctl enable jenkins
+useradd -mG wheel user
 passwd user
 cd /home/user
 sudo -u user wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
@@ -33,6 +34,7 @@ rm -rf yay.tar.gz
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Syu lib32-gcc-libs
+sudo -u user yay -Syu lineageos-devel
 grub-install
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF

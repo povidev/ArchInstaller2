197828535fec58c990d574743256f31c2cc25d21
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-01-09 19:56:22 +0600

    new customizable scripts

--- efi.sh
+++ efi.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
+export DISK="sda"
+export TIMEZONE="Asia/Omsk"
+export ADDITIONALPKG="jenkins htop"
+export ADDITIONALUNITS="jenkins sshd"
+export ADDITIONALYAYPKG="lineageos-devel megatools"
+export SYSTEMLOCALE="en_US"
+export HOSTNAME="arch"
 timedatectl set-ntp true
-parted -s /dev/sda mklabel gpt mkpart primary 2MiB 512MiB mkpart primary 512MiB 100% set 1 esp on
-mkfs.ext4 /dev/sda2
-mkfs.fat -F32 /dev/sda1
-mount /dev/sda2 /mnt
+parted /dev/${DISK} mklabel gpt mkpart primary 2MiB 512MiB mkpart primary 512MiB 100% set 1 esp on
+mkfs.ext4 /dev/${DISK}2
+mkfs.fat -F32 /dev/${DISK}1
+mount /dev/${DISK}2 /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
-pacstrap /mnt base linux linux-firmware networkmanager sudo grub efibootmgr base-devel nano wget openssh jenkins htop
+mount /dev/${DISK}1 /mnt/boot/efi
+pacstrap /mnt base linux linux-firmware networkmanager sudo grub efibootmgr base-devel nano wget openssh ${ADDITIONALPKG}
 genfstab -U /mnt >> /mnt/etc/fstab
-cat << EOF > /mnt/configure.sh
-ln -sf /usr/share/zoneinfo/Asia/Omsk /etc/localtime
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "${SYSTEMLOCALE}.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
+echo "LANG=${SYSTEMLOCALE}.UTF-8" >> /etc/locale.conf
+echo "${HOSTNAME}" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-systemctl enable NetworkManager
-systemctl enable sshd
-systemctl enable jenkins
+systemctl enable NetworkManager ${ADDITIONALUNITS}
 useradd -mG wheel user
 passwd user
 cd /home/user
@@ -29,13 +33,11 @@ sudo -u user tar -xzf yay.tar.gz
 cd yay
 sudo -u user makepkg -si
 cd ..
-rm -rf yay
-rm -rf yay.tar.gz
+rm -rf yay*
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Syu lib32-gcc-libs
-sudo -u user yay -Syu lineageos-devel
-sudo -u user yay -Syu megatools
+sudo -u user yay -Syu ${ADDITIONALYAYPKG}
 grub-install
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF

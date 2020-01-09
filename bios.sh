28d3f7bddaaff748423388195c948d2e92518971
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-01-09 19:54:07 +0600

    new customizable scripts

--- bios.sh
+++ bios.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+export DISK="sda"
+export TIMEZONE="Asia/Omsk"
+export ADDITIONALPKG="jenkins htop"
+export ADDITIONALUNITS="jenkins sshd"
+export ADDITIONALYAYPKG="lineageos-devel megatools"
+export SYSTEMLOCALE="en_US"
+export HOSTNAME="arch"
 timedatectl set-ntp true
-parted -s /dev/sda mklabel msdos mkpart primary 2MiB 100% set 1 boot on
-mkfs.ext4 /dev/sda1
-mount /dev/sda1 /mnt
-pacstrap /mnt base linux linux-firmware networkmanager sudo grub base-devel nano wget openssh
+parted /dev/${DISK} mklabel msdos mkpart primary 2MiB 100% set 1 boot on
+mkfs.ext4 /dev/${DISK}1
+mount /dev/${DISK}1 /mnt
+pacstrap /mnt base linux linux-firmware networkmanager sudo grub base-devel nano wget openssh ${ADDITIONALPKG}
 genfstab -U /mnt >> /mnt/etc/fstab
 cat << EOF > /mnt/configure.sh
-ln -sf /usr/share/zoneinfo/Asia/Omsk /etc/localtime
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "${SYSTEMLOCALE}.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "arch" >> /etc/hostname
+echo "LANG=${SYSTEMLOCALE}.UTF-8" >> /etc/locale.conf
+echo "${HOSTNAME}" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-systemctl enable NetworkManager
-systemctl enable sshd
-useradd -s /bin/bash -mG wheel user
+systemctl enable NetworkManager ${ADDITIONALUNITS}
+useradd -mG wheel user
 passwd user
 cd /home/user
 sudo -u user wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
@@ -24,12 +30,12 @@ sudo -u user tar -xzf yay.tar.gz
 cd yay
 sudo -u user makepkg -si
 cd ..
-rm -rf yay
-rm -rf yay.tar.gz
+rm -rf yay*
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Syu lib32-gcc-libs
-grub-install /dev/sda
+sudo -u user yay -Syu ${ADDITIONALYAYPKG}
+grub-install /dev/${DISK}
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 chmod 755 /mnt/configure.sh

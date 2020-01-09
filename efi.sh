2355b8bab961dc42dbf984706e580974abcbd6d1
Myst33d <45560476+myst33d@users.noreply.github.com>
2020-01-09 11:06:09 +0600

    fix typo

--- efi.sh
+++ efi.sh
@@ -29,7 +29,7 @@ cd yay
 sudo -u user makepkg -si
 cd ..
 rm -rf yay
-rm -rf yay.tar.xz
+rm -rf yay.tar.gz
 echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -Syu lib32-gcc-libs
